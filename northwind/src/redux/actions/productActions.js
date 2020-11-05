e741e8bd819ce3c2318e9cf2e290cc7b6caa8801
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2020-11-05 21:34:04 +0300

    error management

--- northwind/src/redux/actions/productActions.js
+++ northwind/src/redux/actions/productActions.js
@@ -2,18 +2,55 @@ import * as actionTypes from './actionTypes'
 
 const url = "http://localhost:3000/"
 
-export function getProductsSuccess(products){
-    return {type:actionTypes.GET_PRODUCTS_SUCCESS,payload:products}
+export function getProductsSuccess(products) {
+    return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products }
+}
+export function createProductSuccess(product) {
+    return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product }
+}
+export function updateProductSuccess(product) {
+    return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product }
+}
+
+export function saveProductApi(product) {
+    return fetch(url + "products/" + (product.id || ""), { 
+        method: product.id?"PUT":"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify(product)
+    }).then(handleResponse).catch(handleError)
+}
+
+export function saveProduct(product){
+    return function (dispatch) {
+        return saveProductApi(product).then(savedProduct=>{
+            product.id
+            ?dispatch(updateProductSuccess(saveProduct))
+            :dispatch(createProductSuccess(savedProduct))
+        }).catch(error=>{throw error})
+    }
 }
 
 export function getProducts(categoryId) {
     //asenkron operasyonlarda thunk yapısını kullanıyoruz
-    let newUrl = url+"products"
-    if(categoryId){
-         newUrl += "?categoryId="+categoryId
+    let newUrl = url + "products"
+    if (categoryId) {
+        newUrl += "?categoryId=" + categoryId
     }
     return function (dispatch) {
         return fetch(newUrl).then(response => response.json())
             .then(result => dispatch(getProductsSuccess(result)))
     }
+}
+
+export async function handleResponse(response){
+    if(response.ok){
+        return response.json();
+    }
+
+    const error = await response.text()
+    throw new Error(error);
+}
+export function handleError(error){
+    console.error("An error occured!")
+    throw error;
 }
